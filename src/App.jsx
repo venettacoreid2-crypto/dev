5544af2de48da612110210c81489a7d729092ee2
Gito Ismoyo <248871910+venettacoreid2-crypto@users.noreply.github.com>
2026-02-04 23:21:58 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import './styles/App.css';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="app-container">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
